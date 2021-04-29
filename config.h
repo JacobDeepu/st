7838f64dd712396e4d8a41d1a4deaaee37957fce
Jacob Deepu <d2jdeepu@gmail.com>
2021-04-29 22:31:46 +0530

    Use MesloLGS NF font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "MesloLGS NF:antialias=true:hinting=true:pixelsize:12";
 static int borderpx = 2;
 
 /*
