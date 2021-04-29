61847715ebdef7050395de17dc24634049412016
Jacob Deepu <d2jdeepu@gmail.com>
2021-04-29 22:33:20 +0530

    Increase borderpx to 12

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "MesloLGS NF:antialias=true:hinting=true:pixelsize:12";
-static int borderpx = 2;
+static int borderpx = 12;
 
 /*
  * What program is execed by st depends of these precedence rules:
